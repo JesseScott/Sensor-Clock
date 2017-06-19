6a9139cf0a8a16a205653c82daf3a722fd4661e8
Jesse Scott <jesse.scott@eventbase.com>
2017-06-19 08:27:04 -0700

    Basic Onboarding flow.

--- app/src/main/java/tt/co/jesses/sensorclock/SplashActivity.kt
+++ app/src/main/java/tt/co/jesses/sensorclock/SplashActivity.kt
@@ -1,12 +1,55 @@
 package tt.co.jesses.sensorclock
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
+
 
 class SplashActivity : Activity() {
+
+    private val TAG = SplashActivity::class.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val context: Context = applicationContext
+
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        val hasOnboarded = preferences.getBoolean(context.getString(R.string.user_has_seen_onboarding), false)
+
+        //val timer: Thread
+
+        val timer = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(2500)
+
+                    var intent: Intent
+                    if (hasOnboarded) {
+                        intent = Intent(context, MainActivity::class.java)
+                    } else {
+                        intent = Intent(context, OnboardingActivity::class.java)
+                    }
+                    startActivity(intent)
+                    finish()
+                } catch (e: InterruptedException) {
+                    Log.d(TAG, "Error: " + e.localizedMessage)
+                }
+
+            }
+        }
+        timer.start()
+
+
+
+
+
     }
+
+
+
 }
